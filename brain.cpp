970563a52dc3caac392a61b22fc5c03404c82d0a
xSeditx <33107273+xSeditx@users.noreply.github.com>
2018-03-19 00:14:13 -0400

    Update brain.cpp

--- brain.cpp
+++ brain.cpp
@@ -80,8 +80,54 @@ void Net::Think()
         Layers[2].Neurons[OutputCount].Value = Activation(Sum);
     }
 }
-void Net::Draw()
-{ }
+
+
+
+void Net::Draw(int x,int y)
+{
+   int SizeX = 20, 
+       SizeY = 15;
+
+   int X1 = x,
+       Y1 = y, 
+       X2 = 0,
+       Y2 = 0;
+    
+   float WeightValue = 0;
+    
+   for(int Layercount = 1; Layercount < Number_of_Layers; Layercount++)
+    {
+        X1 = (Layercount * SizeX) + x;
+       for(int NeuronCount = 0; NeuronCount < Layers[Layercount].Number_of_Neurons; NeuronCount++)
+       {
+           Y1 = (NeuronCount * SizeY) + y;
+           for(int SynapseCount = 0; SynapseCount <  Layers[Layercount - 1].Number_of_Neurons; SynapseCount++)
+           {
+               Y2 = (SynapseCount * SizeY) + y;
+               X2 = ((Layercount - 1) * SizeX) + x;
+               WeightValue = Layers[Layercount].Neurons[NeuronCount].Synapses[SynapseCount].Weight;
+               
+               if(WeightValue > 0)
+               { 
+                  int Color = WeightValue * 255;
+                      SET_DRAW_COLOR(RGB(0, Color, 0));
+               } 
+               else
+               {
+                  int Color = abs(WeightValue * 255);
+                      SET_DRAW_COLOR(RGB(Color,0,0));
+               }
+               LINE(X1,Y1,X2,Y2);
+               float Mag = 128 * (1+(Layers[Layercount-1].Neurons[SynapseCount].Value));
+               SET_PIXELII(X2,Y2,RGB((int)Mag,(int)Mag,(int)Mag));
+           }
+
+              float Mag = 128 * (1+(Layers[Layercount].Neurons[NeuronCount].Value));
+              SET_PIXELII(X1,Y1,RGB((int)Mag,(int)Mag,(int)Mag));
+       }
+    }
+}
+
 
 
 Synapse::Synapse(float value)
